f00b2eaf34de20f07fbaff5572fc559c4f93174e
gstoica27 <gstoica27@gmail.com>
2018-08-07 11:10:32 -0700

    Added generate structure training file function

--- src/conve/utilities/structure.py
+++ src/conve/utilities/structure.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import, division, print_function
-
+import os
 import numpy as np
 
-__all__ = ['load_adjacency_matrix', 'prune_adjacency_matrix']
+__all__ = ['load_adjacency_matrix', 'prune_adjacency_matrix', 'generate_structure_train_file']
 
 
 def load_adjacency_matrix(path, num_ent):
@@ -28,3 +28,18 @@ def prune_adjacency_matrix(adj_matrix):
     adj_matrix[adj_matrix < avg_num_edges[:, None]] = 0
     adj_matrix[adj_matrix >= avg_num_edges[:, None]] = 1
     return adj_matrix
+
+def generate_structure_train_file(adj_matrix, directory, output_filename):
+    print("Generating Structure Training File")
+    output_file = os.path.join(directory, output_filename)
+    with open(output_file, 'w+') as file:
+        for source_entity in range(len(adj_matrix)):
+            comparison_row = adj_matrix[source_entity]
+            similar_entities = np.where(comparison_row == 1)[0]
+            for entity in similar_entities:
+                train_line = '{0} {1}\n'.format(source_entity, entity)
+                file.write(train_line)
+    return output_file
+
+
+
