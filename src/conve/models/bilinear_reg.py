1769102767b087b814123678148e04f30d31cca5
Otilia Stretcu <otiliastr@gmail.com>
2018-09-26 00:00:37 -0400

    Added Regularization Method 3 Code

--- src/conve/models/bilinear_reg.py
+++ src/conve/models/bilinear_reg.py
@@ -26,7 +26,6 @@ class BiLinearReg(object):
         self.emb_dim = Config['emb_dim']
         self.lr = Config['lr']
         self.summaries = None
-        self.batch_size = 128
         self.weights = self.get_weights()
         self.reg_weight = tf.placeholder(tf.float32)
         self.baseline_weight = tf.placeholder(tf.float32)
@@ -57,6 +56,7 @@ class BiLinearReg(object):
                 'seq_1': tf.int64,
                 'seq_2': tf.int64,
                 'sim': tf.float32,
+                'agg_sim': tf.float32,
                 'seq_mask': tf.int64
                 #'seq_mask_0': tf.int64,
                 #'seq_mask_1': tf.int64,
@@ -69,6 +69,7 @@ class BiLinearReg(object):
                 'seq_2': [None],
                 #'seq': [None, 3],
                 'sim': [None],
+                'agg_sim': [None],
                 #'seq_mask_0': [None],
                 #'seq_mask_1': [None],
                 #'seq_mask_2': [None]})
@@ -94,6 +95,7 @@ class BiLinearReg(object):
         self.reg_seq_2 = self.next_relreg_sample['seq_2']
         #self.reg_seq = self.next_relreg_sample['seq']
         self.reg_sim = self.next_relreg_sample['sim']
+        self.reg_agg_sim = self.next_relreg_sample['agg_sim']
         #self.reg_seq_mask_0 = self.next_relreg_sample['seq_mask_0']
         #self.reg_seq_mask_1 = self.next_relreg_sample['seq_mask_1']
         #self.reg_seq_mask_2 = self.next_relreg_sample['seq_mask_2']
@@ -103,14 +105,20 @@ class BiLinearReg(object):
 
         # DistMult Prediction Pipeline
         # max-margin loss. Shift according to piece-wise tanh
-        self.pos_sim_shift = 1./(1. - self.sim_threshold) * math.pi * (self.reg_sim - self.sim_threshold)
-        self.neg_sim_shift = 1./(self.sim_threshold) * math.pi * (self.reg_sim - self.sim_threshold)
-        self.sim_transform = -tf.tanh(self.pos_sim_shift)
-        self.not_sim_transform = -tf.tanh(self.neg_sim_shift)
-        self.reg_loss_weight = tf.minimum(self.sim_transform, self.not_sim_transform)
+        #self.pos_sim_shift = 1./(1. - self.sim_threshold) * math.pi * (self.reg_sim - self.sim_threshold)
+        #self.neg_sim_shift = 1./(self.sim_threshold) * math.pi * (self.reg_sim - self.sim_threshold)
+        #self.sim_transform = -tf.tanh(self.pos_sim_shift)
+        #self.not_sim_transform = -tf.tanh(self.neg_sim_shift)
+        #self.reg_loss_weight = tf.minimum(self.sim_transform, self.not_sim_transform)
+        #self.condition = tf.greater_equal(self.reg_sim, self.sim_threshold)
+        #self.reg_loss_weight = tf.where(self.condition, tf.ones_like(self.reg_sim)*-1., tf.ones_like(self.reg_sim))
+
+        self.reg_loss_weight = -1. * self.reg_sim 
 
         self.e1_emb = tf.nn.embedding_lookup(self.weights['ent_emb'], self.e1)
         self.rel_emb = tf.nn.embedding_lookup(self.weights['rel_emb'], self.rel)
+        #self.e1_scaled = self.e1_emb / tf.reduce_maximum(self.e1_emb)
+        #self.rel_scaled = self.
         # DistMult Prediction
         self.bilinear_raw_pred = tf.matmul(tf.einsum('bn,bnm->bm', self.e1_emb, self.rel_emb),
                                            self.weights['ent_emb'], transpose_b=True)
@@ -139,6 +147,7 @@ class BiLinearReg(object):
         #self.scanned_tensors = tf.map_fn(lambda x: tf.scan(lambda a, b: tf.matmul(a, b), x), self.reg_seq_embs)
         #self.reg_predictions = tf.boolean_mask(self.scanned_tensors, self.reg_seq_mask)
         batch_size = tf.shape(self.reg_seq_0_emb)[0]
+
         self.reg_seq_0_map = self.reg_seq_0_emb #+ self.reg_seq_0_bias
         self.reg_seq_1_map = tf.matmul(self.reg_seq_0_map, self.reg_seq_1_emb) #+ self.reg_seq_1_bias
         self.reg_seq_2_map = tf.matmul(self.reg_seq_1_map, self.reg_seq_2_emb) #+ self.reg_seq_2_bias
@@ -150,12 +159,19 @@ class BiLinearReg(object):
         self.reg_predictions = tf.boolean_mask(tf.concat([self.is_seq_1, self.is_seq_2, self.is_seq_3], 1), self.reg_seq_mask)
        # self.batch_maps = tf.concat(tensor_maps, 0)
         #self.reg_diffs_flat = tf.reshape(self.reg_rel_embs - self.reg_predictions, [batch_size, self.emb_dim*self.emb_dim])
+        
         # make sure shapes match
-        self.reg_predictions = tf.reshape(self.reg_predictions, [batch_size, self.emb_dim, self.emb_dim])
-        self.reg_rel_embs = tf.reshape(self.reg_rel_embs, [batch_size, self.emb_dim, self.emb_dim])
+        self.reg_predictions = tf.nn.l2_normalize(self.reg_predictions, 1)
+        self.reg_rel_embs = tf.nn.l2_normalize(self.reg_rel_embs, 1)
+        self.reg_predictions = tf.reshape(self.reg_predictions, [batch_size, self.emb_dim * self.emb_dim])
+        self.reg_rel_embs = tf.reshape(self.reg_rel_embs, [batch_size, self.emb_dim * self.emb_dim])
+        # Uncomment below for Cosine Similarity
+        """
         # normalize row bases
         self.reg_pred_normed = tf.nn.l2_normalize(self.reg_predictions, 2)
         self.reg_rel_embs_normed = tf.nn.l2_normalize(self.reg_rel_embs, 2)
+
+
         # compute row-wise cosine similarity
         self.cosine_similarity = tf.reduce_sum(tf.reduce_sum(tf.multiply(self.reg_pred_normed, self.reg_rel_embs_normed), 2), 1)
         # compute loss. Loss is defined as 1 + A' - A component-wise, where A' is dis-similar row-vector to a respective row-vector 
@@ -167,6 +183,21 @@ class BiLinearReg(object):
         #self.reg_batch_loss = tf.reduce_sum(tf.square(self.reg_diffs_flat), 1)
         #self.reg_batch_loss = tf.map_fn(lambda x: tf.reduce_sum(x), tf.square(self.reg_rel_embs - self.reg_predictions))
         #self.reg_loss = tf.reduce_sum(self.reg_batch_loss *  self.reg_loss_weight)
+        """
+        # Uncomment below for similarity loss maps
+         
+        self.W_r_weight = self.reg_sim / self.reg_agg_sim
+        self.W_r_weight = tf.reshape(self.W_r_weight, [batch_size, 1])
+        self.reg_sim_col = tf.reshape(self.reg_sim, [batch_size, 1])
+        self.W_r_scaled = self.W_r_weight * self.reg_rel_embs
+        self.S_scaled = self.reg_predictions * self.reg_sim_col
+        # remove gradient flow
+        self.S_scaled = tf.stop_gradient(self.S_scaled)
+        self.reg_loss = tf.reduce_sum(tf.square(self.W_r_scaled - self.S_scaled))
+        
+
+        #self.e1_scaled = self.e1_emb / tf.reduce_maximum(self.e1_emb)
+        
 
         # Optimization
         #self.collective_loss = self.bilinear_loss
@@ -180,6 +211,7 @@ class BiLinearReg(object):
                                              [self.num_ent, self.emb_dim],
                                              dtype=tf.float32,
                                              initializer=tf.contrib.layers.xavier_initializer())
+        """
         weights['rel_emb_1'] = tf.get_variable('rel_embeddings_1',
                                              [self.num_rel, self.emb_dim, 8],
                                              dtype=tf.float32,
@@ -189,9 +221,15 @@ class BiLinearReg(object):
                                              dtype=tf.float32,
                                              initializer=tf.contrib.layers.xavier_initializer())
         weights['rel_emb'] = tf.matmul(weights['rel_emb_1'], weights['rel_emb_2'])
-        #weights['rel_bias'] = tf.get_variable('rel_bias',
+        """
+        weights['rel_emb'] = tf.get_variable('rel_embeddings',
+                                             [self.num_rel, self.emb_dim, self.emb_dim],
+                                             dtype=tf.float32,
+                                             initializer=tf.contrib.layers.xavier_initializer())
+         #weights['rel_bias'] = tf.get_variable('rel_bias',
          #                                    [self.num_rel, self.emb_dim, self.emb_dim],
           #                                   dtype=tf.float32,
            #                                  initializer=tf.contrib.layers.xavier_initializer())
+
         return weights
 
