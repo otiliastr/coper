3f69c87c3a197f858c83f9b7bbad14481f8d2316
Anthony Platanios <e.a.platanios@gmail.com>
2018-10-28 17:15:17 -0400

    Minor edits.

--- src/conve/cpg/conve_experiment.py
+++ src/conve/cpg/conve_experiment.py
@@ -11,13 +11,13 @@ from ..evaluation.metrics import ranking_and_hits
 
 LOGGER = logging.getLogger(__name__)
 
-DATA_LOADER = KinshipLoader()
+DATA_LOADER = FB15k237Loader()
 
 ENT_EMB_SIZE = 200
-REL_EMB_SIZE = 200
+REL_EMB_SIZE = 50
 CONCAT_REL = False      # Set to `False` for plain ConvE
-CONTEXT_REL_CONV = []   # Set to `None` for plain ConvE
-CONTEXT_REL_OUT = None  # Set to `None` for plain ConvE
+CONTEXT_REL_CONV = None # Set to `None` for plain ConvE
+CONTEXT_REL_OUT = []    # Set to `None` for plain ConvE
 
 CONTEXT_REL_DROPOUT = 0.5
 CONTEXT_REL_USE_BATCH_NORM = True
@@ -26,7 +26,7 @@ INPUT_DROPOUT = 0.2
 FEATURE_MAP_DROPOUT = 0.3
 OUTPUT_DROPOUT = 0.2
 LEARNING_RATE = 1e-3
-BATCH_SIZE = 128
+BATCH_SIZE = 512
 LABEL_SMOOTHING_EPSILON = 0.1
 
 DEVICE = '/GPU:0'
