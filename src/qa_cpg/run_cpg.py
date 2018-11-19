9055ff344c0092d6df6322b7a676037b87575fc7
Otilia Stretcu <otiliastr@gmail.com>
2018-11-19 12:17:08 -0500

    Refactored run script to use a config file.

--- src/qa_cpg/run_cpg.py
+++ src/qa_cpg/run_cpg.py
@@ -2,12 +2,13 @@ from __future__ import absolute_import, division, print_function
 
 import logging
 import os
-
 import tensorflow as tf
+import yaml
 
 from qa_cpg import data
-from qa_cpg.conve import ConvE
+from qa_cpg.models import ConvE
 from qa_cpg.metrics import ranking_and_hits
+from qa_cpg.utils.dict_with_attributes import AttributeDict
 
 logger = logging.getLogger(__name__)
 
@@ -15,118 +16,105 @@ logger = logging.getLogger(__name__)
 def _evaluate(data_iterator, data_iterator_handle, name, summary_writer, step):
     logger.info('Running %s...' % name)
     session.run(data_iterator.initializer)
-    mr, mrr, hits = ranking_and_hits(model, EVAL_PATH, data_iterator_handle, name, session)
+    mr, mrr, hits = ranking_and_hits(model, eval_path, data_iterator_handle, name, session)
 
-    if SUMMARY_STEPS is not None:
+    if cfg.eval.summary_steps is not None:
         summary = tf.Summary()
         for hits_level, hits_value in hits.items():
             summary.value.add(tag=name+'/hits@'+str(hits_level), simple_value=hits_value)
         summary.value.add(tag=name+'/mrr', simple_value=mrr)
+        summary.value.add(tag=name+'/mr', simple_value=mr)
         summary_writer.add_summary(summary, step)
         summary_writer.flush()
 
-data_loader = data.KinshipLoader()
+# Parameters.
+use_cpg = True
 
-ENT_EMB_SIZE = 200
-REL_EMB_SIZE = 200
-CONCAT_REL = False      # Set to `False` for plain ConvE
-CONTEXT_REL_CONV = None # Set to `None` for plain ConvE
-CONTEXT_REL_OUT = None    # Set to `None` for plain ConvE
+# Load data.
+data_loader = data.NationsLoader()
 
-CONTEXT_REL_DROPOUT = 0.2
-CONTEXT_REL_USE_BATCH_NORM = True
+# Load configuration parameters.
+model_descr = 'cpg' if use_cpg else 'conve'
+config_path = 'qa_cpg/configs/config_kinship_%s.yaml' % model_descr
+with open(config_path, 'r') as file:
+    cfg = yaml.load(file)
+print(cfg)
+cfg = AttributeDict(cfg)
 
-INPUT_DROPOUT = 0.2
-FEATURE_MAP_DROPOUT = 0.3
-OUTPUT_DROPOUT = 0.2
-LEARNING_RATE = 1e-3
-BATCH_SIZE = 512
-LABEL_SMOOTHING_EPSILON = 0.1
+# Compose model name based on config params.
+model_name = '{}-{}-ent_emb_{}-rel_emb_{}-batch_{}'.format(
+    model_descr,
+    data_loader.dataset_name,
+    cfg.model.entity_embedding_size,
+    cfg.model.relation_embedding_size,
+    cfg.training.batch_size)
+suffix = '-context_batchnorm_{}'.format(cfg.context.context_rel_use_batch_norm) if use_cpg else ''
+model_name += suffix
 
-DEVICE = '/GPU:0'
-MAX_STEPS = 10000000
-LOG_STEPS = 100
-CKPT_STEPS = 1000
-EVAL_STEPS = 1000
-LOG_LOSS = 100
-SUMMARY_STEPS = EVAL_STEPS
-
-EVAL_ON_TRAIN = False
-EVAL_ON_DEV = True
-EVAL_ON_TEST = True
-
-ADD_LOSS_SUMMARIES = True
-ADD_VARIABLE_SUMMARIES = False
-ADD_TENSOR_SUMMARIES = False
-
-MODEL_NAME = 'cpg-{}-ent_emb_{}-rel_emb_{}-batch_{}'.format(
-    data_loader.dataset_name, ENT_EMB_SIZE, REL_EMB_SIZE, BATCH_SIZE)
-
-WORKING_DIR = os.path.join(os.getcwd(), 'temp')
-DATA_DIR = os.path.join(WORKING_DIR, 'data')
-LOG_DIR = os.path.join(WORKING_DIR, 'models', MODEL_NAME, 'logs')
-SUMMARIES_DIR = os.path.join(WORKING_DIR, 'summaries', MODEL_NAME)
-CKPT_DIR = os.path.join(WORKING_DIR, 'checkpoints', MODEL_NAME, 'model_weights.ckpt')
-if not os.path.exists(CKPT_DIR):
-    os.makedirs(CKPT_DIR)
-CKPT_PATH = os.path.join(CKPT_DIR, 'model_weights.ckpt')
-EVAL_PATH = os.path.join(WORKING_DIR, 'evaluation', MODEL_NAME)
-
-os.makedirs(EVAL_PATH, exist_ok=True)
+# Create directories for saving downloaded data, summaries, logs and checkpoints.
+working_dir = os.path.join(os.getcwd(), 'temp')
+data_dir = os.path.join(working_dir, 'data')
+log_dir = os.path.join(working_dir, 'models', model_name, 'logs')
+summaries_dir = os.path.join(working_dir, 'summaries', model_name)
+ckpt_dir = os.path.join(working_dir, 'checkpoints', model_name, 'model_weights.ckpt')
+os.makedirs(ckpt_dir, exist_ok=True)
+ckpt_path = os.path.join(ckpt_dir, 'model_weights.ckpt')
+eval_path = os.path.join(working_dir, 'evaluation', model_name)
+os.makedirs(eval_path, exist_ok=True)
 
 if __name__ == '__main__':
-    data_loader.create_tf_record_files(DATA_DIR)
+    data_loader.create_tf_record_files(data_dir)
 
     # Create the model.
-    with tf.device(DEVICE):
+    with tf.device(cfg.training.device):
         # We are using resource variables because due to some implementation details, this allows us to
         # better utilize GPUs while training.
         with tf.variable_scope('variables', use_resource=True):
             model = ConvE(model_descriptors={
-                'label_smoothing_epsilon': LABEL_SMOOTHING_EPSILON,
+                'label_smoothing_epsilon': cfg.model.label_smoothing_epsilon,
                 'num_ent': data_loader.num_ent,
                 'num_rel': data_loader.num_rel,
-                'ent_emb_size': ENT_EMB_SIZE,
-                'rel_emb_size': REL_EMB_SIZE,
-                'concat_rel': CONCAT_REL,
-                'context_rel_conv': CONTEXT_REL_CONV,
-                'context_rel_out': CONTEXT_REL_OUT,
-                'context_rel_dropout': CONTEXT_REL_DROPOUT,
-                'context_rel_use_batch_norm': CONTEXT_REL_USE_BATCH_NORM,
-                'input_dropout': INPUT_DROPOUT,
-                'hidden_dropout': FEATURE_MAP_DROPOUT,
-                'output_dropout': OUTPUT_DROPOUT,
-                'learning_rate': LEARNING_RATE,
-                'batch_size': BATCH_SIZE,
-                'add_loss_summaries': ADD_LOSS_SUMMARIES,
-                'add_variable_summaries': ADD_VARIABLE_SUMMARIES,
-                'add_tensor_summaries': ADD_TENSOR_SUMMARIES})
+                'ent_emb_size': cfg.model.entity_embedding_size,
+                'rel_emb_size': cfg.model.relation_embedding_size,
+                'concat_rel': cfg.model.concat_rel,
+                'context_rel_conv': cfg.context.context_rel_conv,
+                'context_rel_out':  cfg.context.context_rel_out,
+                'context_rel_dropout': cfg.context.context_rel_dropout,
+                'context_rel_use_batch_norm': cfg.context.context_rel_use_batch_norm,
+                'input_dropout': cfg.model.input_dropout,
+                'hidden_dropout': cfg.model.feature_map_dropout,
+                'output_dropout': cfg.model.output_dropout,
+                'learning_rate': cfg.training.learning_rate,
+                'batch_size': cfg.training.batch_size,
+                'add_loss_summaries': cfg.eval.add_loss_summaries,
+                'add_variable_summaries': cfg.eval.add_variable_summaries,
+                'add_tensor_summaries': cfg.eval.add_tensor_summaries})
 
     # Create dataset iterator initializers.
     train_dataset = data_loader.train_dataset(
-        directory=DATA_DIR,
-        batch_size=BATCH_SIZE,
+        directory=data_dir,
+        batch_size=cfg.training.batch_size,
         include_inv_relations=True,
         buffer_size=1024,
         prefetch_buffer_size=16)
     train_eval_dataset = data_loader.eval_dataset(
-        directory=DATA_DIR,
+        directory=data_dir,
         dataset_type='train',
-        batch_size=BATCH_SIZE,
+        batch_size=cfg.training.batch_size,
         include_inv_relations=False,
         buffer_size=1024,
         prefetch_buffer_size=16)
     dev_eval_dataset = data_loader.eval_dataset(
-        directory=DATA_DIR,
+        directory=data_dir,
         dataset_type='dev',
-        batch_size=BATCH_SIZE,
+        batch_size=cfg.training.batch_size,
         include_inv_relations=False,
         buffer_size=1024,
         prefetch_buffer_size=16)
     test_eval_dataset = data_loader.eval_dataset(
-        directory=DATA_DIR,
+        directory=data_dir,
         dataset_type='test',
-        batch_size=BATCH_SIZE,
+        batch_size=cfg.training.batch_size,
         include_inv_relations=False,
         buffer_size=1024,
         prefetch_buffer_size=16)
@@ -143,7 +131,7 @@ if __name__ == '__main__':
     # Create a TensorFlow session and start training.
     session = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
     saver = tf.train.Saver()
-    summary_writer = tf.summary.FileWriter(SUMMARIES_DIR, session.graph)
+    summary_writer = tf.summary.FileWriter(summaries_dir, session.graph)
 
     # Initialize the values of all variables and the train dataset iterator.
     session.run(tf.global_variables_initializer())
@@ -154,12 +142,12 @@ if __name__ == '__main__':
     dev_eval_iterator_handle = session.run(dev_eval_iterator.string_handle())
     test_eval_iterator_handle = session.run(test_eval_iterator.string_handle())
 
-    for step in range(MAX_STEPS):
+    for step in range(cfg.training.max_steps):
         feed_dict = {
             model.is_train: True,
             model.input_iterator_handle: train_iterator_handle}
 
-        if model.summaries is not None and SUMMARY_STEPS is not None and step % SUMMARY_STEPS == 0:
+        if model.summaries is not None and cfg.eval.summary_steps is not None and step % cfg.eval.summary_steps == 0:
             summaries, loss, _ = session.run((model.summaries, model.loss, model.train_op), feed_dict)
             summary_writer.add_summary(summaries, step)
             summary_writer.flush()
@@ -167,19 +155,19 @@ if __name__ == '__main__':
             loss, _ = session.run((model.loss, model.train_op), feed_dict)
 
         # Log the loss, if necessary.
-        if step % LOG_STEPS == 0:
+        if step % cfg.eval.log_steps == 0:
             logger.info('Step %6d | Loss: %10.4f', step, loss)
 
         # Evaluate, if necessary.
-        if step % EVAL_STEPS == 0:
+        if step % cfg.eval.eval_steps == 0:
             # Perform evaluation.
-            if EVAL_ON_TRAIN:
+            if cfg.eval.eval_on_train:
                 _evaluate(train_eval_iterator, train_eval_iterator_handle, 'train_evaluation', summary_writer, step)
-            if EVAL_ON_DEV:
+            if cfg.eval.eval_on_dev:
                 _evaluate(dev_eval_iterator, dev_eval_iterator_handle, 'dev_evaluation', summary_writer, step)
-            if EVAL_ON_TEST:
+            if cfg.eval.eval_on_test:
                 _evaluate(test_eval_iterator, test_eval_iterator_handle, 'test_evaluation', summary_writer, step)
 
-        if step % CKPT_STEPS == 0:
-            logger.info('Saving checkpoint at %s.', CKPT_PATH)
-            saver.save(session, CKPT_PATH)
+        if step % cfg.eval.ckpt_steps == 0:
+            logger.info('Saving checkpoint at %s.', ckpt_path)
+            saver.save(session, ckpt_path)
