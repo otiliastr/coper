68d1b4620324ba3de1546faf8ad80d05e9dc41cd
gstoica27 <gstoica27@gmail.com>
2019-03-06 22:53:47 -0500

    fixed error

--- src/emb/fact_network.py
+++ src/emb/fact_network.py
@@ -360,8 +360,8 @@ class CPG_ConvE(nn.Module):
             # X = nn.functional.linear(input=X,
             #                          weight=self.fc_weights(R))
             fc_weights = self.fc_weights(R)
-            X = X.matmul(fc_weights)
-            print("X shape before matmul: {} | fc_Weights shape: {}".format(X.size(), fc_weights.size()))
+            # X = X.matmul(fc_weights)
+            X = torch.einsum('ij, ijk-> ik', X, fc_weights)
             print("X shape after matmul: {}".format(X.size()))
             X += self.fc_bias(R)
             print("X shape: {}".format(X.size()))
