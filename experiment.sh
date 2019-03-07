b8aaddf61a6685019610b6f2f9e458d29573701a
gstoica27 <gstoica27@gmail.com>
2019-03-06 21:41:18 -0500

    added cpg commangline args

--- experiment.sh
+++ experiment.sh
@@ -23,38 +23,44 @@ fi
 
 cmd="python3 -m src.experiments \
     --data_dir $data_dir \
-    --gpu $gpu"
-#    $ARGS
-#    $exp \
-#    --model $model \
-#    --bandwidth $bandwidth \
-#    --entity_dim $entity_dim \
-#    --relation_dim $relation_dim \
-#    --history_dim $history_dim \
-#    --history_num_layers $history_num_layers \
-#    --num_rollouts $num_rollouts \
-#    --num_rollout_steps $num_rollout_steps \
-#    --bucket_interval $bucket_interval \
-#    --num_epochs $num_epochs \
-#    --num_wait_epochs $num_wait_epochs \
-#    --num_peek_epochs $num_peek_epochs \
-#    --batch_size $batch_size \
-#    --train_batch_size $train_batch_size \
-#    --dev_batch_size $dev_batch_size \
-#    --margin $margin \
-#    --learning_rate $learning_rate \
-#    --baseline $baseline \
-#    --grad_norm $grad_norm \
-#    --emb_dropout_rate $emb_dropout_rate \
-#    --ff_dropout_rate $ff_dropout_rate \
-#    --action_dropout_rate $action_dropout_rate \
-#    --action_dropout_anneal_interval $action_dropout_anneal_interval \
-#    $relation_only_flag \
-#    --beta $beta \
-#    --beam_size $beam_size \
-#    --num_paths_per_entity $num_paths_per_entity \
-#    $group_examples_by_query_flag \
-#    $use_action_space_bucketing_flag \
+    $exp \
+    --model $model \
+    --bandwidth $bandwidth \
+    --entity_dim $entity_dim \
+    --relation_dim $relation_dim \
+    --history_dim $history_dim \
+    --history_num_layers $history_num_layers \
+    --num_rollouts $num_rollouts \
+    --num_rollout_steps $num_rollout_steps \
+    --bucket_interval $bucket_interval \
+    --num_epochs $num_epochs \
+    --num_wait_epochs $num_wait_epochs \
+    --num_peek_epochs $num_peek_epochs \
+    --batch_size $batch_size \
+    --train_batch_size $train_batch_size \
+    --dev_batch_size $dev_batch_size \
+    --margin $margin \
+    --learning_rate $learning_rate \
+    --baseline $baseline \
+    --grad_norm $grad_norm \
+    --emb_dropout_rate $emb_dropout_rate \
+    --ff_dropout_rate $ff_dropout_rate \
+    --action_dropout_rate $action_dropout_rate \
+    --action_dropout_anneal_interval $action_dropout_anneal_interval \
+    --hidden_dropout_rate $hidden_dropout_rate \
+    --feat_dropout_rate $feat_dropout_rate \
+    --cpg_conv_net $cpg_conv_net \
+    --cpg_fc_net $cpg_fc_net \
+    --cpg_batch_norm $cpg_batch_norm \
+    --cpg_batch_norm_momentum $cpg_batch_norm_momentum \
+    $relation_only_flag \
+    --beta $beta \
+    --beam_size $beam_size \
+    --num_paths_per_entity $num_paths_per_entity \
+    $group_examples_by_query_flag \
+    $use_action_space_bucketing_flag \
+    --gpu $gpu \
+    $ARGS"
 
 echo "Executing $cmd"
 
