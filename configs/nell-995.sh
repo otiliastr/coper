2186e12051b618c57b3d37d7dfd6f4be1c3f4bf6
gstoica27 <gstoica27@gmail.com>
2019-05-16 17:42:58 -0400

    changed nell config

--- configs/nell-995.sh
+++ configs/nell-995.sh
@@ -29,6 +29,15 @@ action_dropout_anneal_interval=1000
 beta=0.05
 relation_only="False"
 beam_size=128
+# CPG Args. -1: No CPG, anything else: CPG
+# Network Structure: 1 2 3 --> [1, 2, 3]
+# Nothing = [] due to nargs='*'
+# in parse args
+pg_network_structure=
+pg_dropout=.2
+pg_batch_norm=True
+pg_batch_norm_momentum=.1
+pg_use_bias=False
 
 num_paths_per_entity=-1
 margin=-1
