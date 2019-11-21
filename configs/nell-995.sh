a44c56f65fea8201643005ea3003400167e87a3d
George I Stoica <gis@gpu2.learning.cs.cmu.edu>
2019-11-20 21:20:38 -0500

    best param settings yet

--- configs/nell-995.sh
+++ configs/nell-995.sh
@@ -12,7 +12,7 @@ history_dim=200
 history_num_layers=3
 num_rollouts=20
 num_rollout_steps=3
-num_epochs=15
+num_epochs=20
 num_wait_epochs=100
 num_peek_epochs=2
 bucket_interval=5
@@ -28,7 +28,7 @@ action_dropout_rate=0.3
 action_dropout_anneal_interval=1000
 beta=0.05
 relation_only="False"
-beam_size=128
+beam_size=512
 # CPG Args. -1: No CPG, anything else: CPG
 # Network Structure: 1 2 3 --> [1, 2, 3]
 # Nothing = [] due to nargs='*'
@@ -37,7 +37,7 @@ pg_network_structure=
 pg_dropout=0.2
 pg_batch_norm=True
 pg_batch_norm_momentum=.1
-pg_use_bias=False
+pg_use_bias=True
 
 num_paths_per_entity=-1
 margin=-1
